2ce3ede2cec2bab4a981c7e3001d3a72fb0fdaf0
David Sass <elethulyelenseg@hotmail.com>
2020-07-25 21:09:38 +0200

    feat(PSWinTerminal): Get-WT*
    Get-WTCurrentTheme
    Get-WTTheme
    
    Enjoy

--- PSWinTerminal.psd1
+++ PSWinTerminal.psd1
@@ -78,16 +78,16 @@ Description = 'PowerShell module to manipulate Windows Terminal'
 # NestedModules = @()
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-FunctionsToExport = @()
+FunctionsToExport = @('Get-WTCurrentTheme','Get-WTTheme')
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
-CmdletsToExport = @()
+#CmdletsToExport = @()
 
 # Variables to export from this module
-VariablesToExport = '*'
+#VariablesToExport = '*'
 
 # Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
-AliasesToExport = @()
+#AliasesToExport = @()
 
 # DSC resources to export from this module
 # DscResourcesToExport = @()
---
+++ PSWinTerminal.psm1
@@ -0,0 +1,59 @@
+
+if ($env:WT_SESSION) {
+    $Script:PSWinTerminalConfigPath = "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState\settings.json"
+    $Script:PSWinTerminalDefaultsPath = "C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.1.2021.0_x64__8wekyb3d8bbwe\defaults.json"
+
+    if ( ( Test-Path -LiteralPath $Script:PSWinTerminalDefaultsPath) ) {
+        $Script:PSWinTerminalDefaults = (Get-Content -LiteralPath $Script:PSWinTerminalDefaultsPath | Where-Object { -not $_.Trim().StartsWith('//') } ) | ConvertFrom-Json
+        $Script:PSWinTerminalDefaultThemes = $Script:PSWinTerminalDefaults.schemes.name
+    }
+    if ( ( Test-Path -LiteralPath $Script:PSWinTerminalConfigPath) ) {
+        $Script:PSWinTerminalConfig = (Get-Content -LiteralPath $Script:PSWinTerminalConfigPath | Where-Object { -not $_.Trim().StartsWith('//') } ) | ConvertFrom-Json
+        $Script:PSWinTerminalThemes = $Script:PSWinTerminalConfig.schemes.name
+        $Script:PSWinTerminalCurentProfile = $Script:PSWinTerminalConfig.profiles.list.GetEnumerator() | Where-Object { $_.guid -eq $env:WT_PROFILE_ID}
+    }
+
+    function Get-WTCurrentTheme {
+        [CmdletBinding()]
+        param (
+        )
+        begin {
+        }
+
+        process {
+            if ($Script:PSWinTerminalCurentProfile.colorScheme) {
+                $Script:PSWinTerminalCurentProfile.colorScheme
+            }
+            else {
+                Write-Host "Campbell (Default)"
+            }
+        }
+        
+        end {
+            
+        }
+    }
+
+    function Get-WTTheme {
+        [CmdletBinding()]
+        param (
+        )
+
+        begin {
+
+        }
+
+        process {
+            $WTThemes = New-Object System.Collections.ArrayList
+            $Script:PSWinTerminalDefaultThemes | ForEach-Object { $null = $WTThemes.Add("$_") }
+            $Script:PSWinTerminalThemes | ForEach-Object { $null = $WTThemes.Add("$_ *") }
+            $WTThemes.ToArray() | Sort-Object
+        }
+
+        end {
+
+        }
+    }
+
+    Export-ModuleMember -Function *
+}
